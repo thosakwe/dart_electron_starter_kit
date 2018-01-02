f1239abc32348d034d7a4a6cc3aa066192d241f1
thosakwe <thosakwe@gmail.com>
2018-01-02 15:19:06 -0500

    dart:async and loadLibrary work

--- web/main.dart
+++ web/main.dart
@@ -1,11 +1,19 @@
+import 'dart:async';
 import 'dart:html';
-import 'package:string_scanner/string_scanner.dart';
+import 'package:string_scanner/string_scanner.dart' deferred as string_scanner;
 
 void main() {
-  var scanner = new StringScanner('hello world');
-  scanner.expect('hello');
-  print(scanner.lastMatch.pattern);
   var h1 = new Element.tag('h1')..text = 'Running DDC within Electron!';
   var i = new Element.tag('i')..text = 'Boldly go where no Dartisan has gone before.';
   document.body.children.addAll([h1, i]);
+
+  string_scanner.loadLibrary().then((_) {
+    var scanner = new string_scanner.StringScanner('hello world');
+    scanner.expect('hello');
+    print(scanner.lastMatch.pattern);
+  });
+
+  new Timer(const Duration(seconds: 2), () {
+    print('Hello, dart:async!');
+  });
 }
